9be26bef7b3de14fd29bbacb53271225099bc945
Shivam-TyagiG <91339657+Shivam-TyagiG@users.noreply.github.com>
2022-10-18 11:45:54 +0530

    Time: 39 ms (68.57%), Space: 31.1 MB (51.62%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        
+        int total  =  0;
+        for(auto num : nums)
+        {
+            total =  total + num;
+        }
+        cout<<total<<endl;
+        int curr=0,count =0;
+        for(auto num : nums)
+        {
+            
+            
+            if(curr*2 == total-num){
+                return count;
+            }
+            count++;
+            curr = curr+num;
+        }
+        return -1;
+    }
+};
