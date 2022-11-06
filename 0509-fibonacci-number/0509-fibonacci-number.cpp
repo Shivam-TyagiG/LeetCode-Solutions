909f80b6f3cea874dc3860c290d1a9c652e907d8
Shivam Tyagi <91339657+Shivam-TyagiG@users.noreply.github.com>
2022-11-06 06:03:04 +0530

    Time: 18 ms (38.99%), Space: 6 MB (38.07%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,45 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-        /*
-        
-        //Solved by recursion 
-        if(n==0){
+        if(n==0)
+        {
             return 0;
         }
-        if(n==1){
+        if(n==1)
+        {
             return 1;
         }
-        int ans =fib(n-1)+fib(n-2);
-        return ans;*/
-        
-        //solved by for loop 
-        
-        if(n==0){           //return ans if n==0 or n==1
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-        
-        //allocate heap(dynamic) memory to store data
-        int *arr=new int[n+2];
-        arr[0]=0;
-        arr[1]=1;
-        
-        //for loop upto n terms
-        for(int i=2;i<=n;i++){
-            arr[i]=arr[i-1]+arr[i-2];
-        }
-        
-        //store the answer in static memory
-        int ans=arr[n];
-        
-        //delete the dynamic memory which is allocate for array 
-        delete []arr;
-        
-        //return ans
-        return ans;
+        return fib(n-1)+fib(n-2);
     }
-    
 };
