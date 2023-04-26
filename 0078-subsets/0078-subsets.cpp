658a871b269f78969600ccce9955bacf7e10960b
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2023-04-26 16:00:55 +0530

    Time: 0 ms (100.00%), Space: 7 MB (97.11%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void solve(vector<int>& nums, vector<vector<int>>& ans, vector<int> output, int i){
+    void solve(vector<int>& nums, vector<vector<int>>& ans, vector<int>& output, int i){
         if(i==nums.size()){
             ans.push_back(output);
             return ;
