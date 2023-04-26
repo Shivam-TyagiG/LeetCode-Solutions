226ab47f0481ea475a4434362e52148ac3983be2
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2023-04-26 11:48:02 +0530

    Time: 50 ms (23.37%), Space: 21 MB (13.98%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int solve(vector<int>& satisfaction, int i, int time, vector<vector<int>>& dp ){
+        if(i==satisfaction.size()) return 0;
+        if(dp[i][time]!=-1) return dp[i][time];
+        int include = (time+1)*satisfaction[i]+solve(satisfaction, i+1, time+1, dp);
+        int exclude = solve(satisfaction, i+1, time, dp);
+        return  dp[i][time]=max(include, exclude);
+    }
+    int maxSatisfaction(vector<int>& satisfaction) {
+        sort(satisfaction.begin(), satisfaction.end());
+        int n = satisfaction.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        return solve(satisfaction, 0, 0, dp);
+    }
+};
