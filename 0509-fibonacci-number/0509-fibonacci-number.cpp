06fbd52f901ba10e41f0b8a0c4ab77dce54f0a60
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2023-05-05 14:34:28 +0530

    Time: 9 ms (48.81%), Space: 5.9 MB (94.22%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0)
-        {
-            return 0;
-        }
-        if(n==1)
-        {
-            return 1;
-        }
-        return fib(n-1)+fib(n-2);
+        
+        //write base case 
+        //where recursion stop
+        if(n<=1)
+            return n;
+        //now start processing to calculate result
+        //recursive call 
+        int prev = fib(n-1);
+        int prev_of_prev = fib(n-2);
+
+        return  prev + prev_of_prev;
     }
 };
