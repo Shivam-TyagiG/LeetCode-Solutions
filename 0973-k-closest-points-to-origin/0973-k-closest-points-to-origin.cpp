d5b1f03a9870b046b032a771e3d364d0d49e6ed0
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2023-07-05 10:59:51 +0530

    Time: 289 ms (48.68%), Space: 63.8 MB (77.07%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,15 +1,16 @@
-bool cmp(vector<int>& a, vector<int>& b){
-        return (a[0]*a[0]+a[1]*a[1])<(b[0]*b[0]+b[1]*b[1]);
+bool cmp(vector<int> &a, vector<int> &b){
+        return ((a[0]*a[0]+a[1]*a[1])<(b[0]*b[0]+b[1]*b[1]));
     }
-
 class Solution {
 public:
-    
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         sort(points.begin(), points.end(), cmp);
         vector<vector<int>> ans;
-        for(int i=0; i<k; i++){
-            ans.push_back(points[i]);
+        int count = 0;
+        for(auto a : points){
+            count++;
+            ans.push_back(a);
+            if(count==k) break;
         }
         return ans;
     }
