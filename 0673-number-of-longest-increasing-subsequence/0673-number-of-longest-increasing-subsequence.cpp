82cb11bb651c6e2870e11adc14a7fa0b121e6823
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2023-07-21 23:13:55 +0530

    Time: 175 ms (79.01%), Space: 13.1 MB (66.65%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -1,25 +1,46 @@
 class Solution {
 public:
-    int findNumberOfLIS(vector<int>& nums) {
-        int n = nums.size(), maxlen = 1, ans = 0;
-        vector<int> cnt(n, 1), len(n, 1);
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    if (len[j]+1 > len[i]) {
-                        len[i] = len[j]+1;
-                        cnt[i] = cnt[j];
-                    }
-                    else if (len[j]+1 == len[i]) 
-                        cnt[i] += cnt[j];
-                }
-            }
-            maxlen = max(maxlen, len[i]);
+    vector<int> dp; //dp[i]=length of the LIS ending at index i
+    vector<int> count;// # of LIS with length=dp[i]
+
+    void LIS(int i, vector<int>& nums){
+        if (i==0){
+            dp[0]=1;
+            count[0]=1;
+            return ;
         }
-        // find the longest increasing subsequence of the whole sequence
-       // sum valid counts
-        for (int i = 0; i < n; i++) 
-            if (len[i] == maxlen) ans += cnt[i];
+        if (count[i]!=-1) return;
+        dp[i]=1;
+        count[i]=1;
+        for(int j=0; j<i; j++){
+            if(nums[i]>nums[j]){
+                LIS(j, nums);
+                if (dp[i]<dp[j]+1) {
+                    dp[i]=dp[j]+1;
+                    count[i]=count[j];
+                }
+                else if (dp[i]==dp[j]+1){
+                    count[i]+=count[j];   
+                }    
+            }      
+        }
+    }
+
+    int findNumberOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        dp.assign(n,-1);
+        count.assign(n, -1);
+        for(int i=0; i<n; i++)
+            LIS(i, nums);
+        int M=*max_element(dp.begin(), dp.end());
+    //    cout<<M<<endl;
+        int ans=0;
+        for (int i=0; i<n; i++){
+    //        cout<<dp[i]<<",";
+            if (M==dp[i])
+                ans+=count[i];
+        }   
+    //    cout<<"\n---\nans="<<ans<<endl;
         return ans;
     }
 };
