2ec8864b37632e5a0e8680973f71e357a3dc933a
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2023-08-04 11:58:38 +0530

    Time: 904 ms (25.20%), Space: 289.2 MB (15.02%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,67 +1,41 @@
 class Solution {
-    private:
-    void merge(vector<int> &arr,int s,int e)
-    {
-        int mid  = s+(e-s)/2;
-        int len1 = mid-s+1;
-        int len2 = e-mid;
-        int* first = new int[len1];
-        int* second=  new int[len2];
-        
-        //copy the elemet;
-        int k=s;
-        for(int i=0;i<len1;i++)
-        {
-            first[i]=arr[k++];
-        }
-        k=mid+1;
-        for(int i=0;i<len2;i++)
-        {
-            second[i]=arr[k++];
-        }
-        
-        //merge 2 sorted array
-        int index1 = 0;
-        int index2 = 0;
-        int mainarrayindex = s;
-        while(index1<len1 && index2<len2)
-        {
-            if(first[index1] < second[index2])
-            {
-                arr[mainarrayindex++] = first[index1++];
+public:
+    void conquer(vector<int>& nums, int i, int mid, int j){
+        int first = i, second = mid+1;
+        vector<int> arr;
+        while(first<=mid && second<=j){
+            if(nums[first]<=nums[second]){
+                arr.push_back(nums[first]);
+                first++;
             }
             else{
-                arr[mainarrayindex++] = second[index2++];
+                arr.push_back(nums[second]);
+                second++;
             }
         }
         
-        //copy the remaining element
-        while(index1<len1)
-        {
-            arr[mainarrayindex++]=first[index1++];
+        while(first<=mid){
+            arr.push_back(nums[first]);
+            first++;
         }
-        while(index2<len2)
-        {
-            arr[mainarrayindex++]  = second[index2++];
+        while(second<=j){
+            arr.push_back(nums[second]);
+            second++;
+        }
+        
+        for(int k=i; k<=j; k++){
+            nums[k]=arr[k-i];
         }
     }
-    private:
-    void MergeSort(vector<int> & nums,int s,int e)
-    {
-        if(s>=e)
-        {
-            return ;
-        }
-        int mid  =  s+(e-s)/2;
-        MergeSort(nums,s,mid);
-        MergeSort(nums,mid+1,e);
-        merge(nums,s,e);
+    void divide(vector<int>& nums, int i, int j){
+        if(i>=j) return ;
+        int mid = i+(j-i)/2;
+        divide(nums, i, mid);
+        divide(nums, mid+1, j);
+        conquer(nums, i, mid, j);
     }
-public:
     vector<int> sortArray(vector<int>& nums) {
-        int s= 0 ;
-        int e = nums.size()-1;
-        MergeSort(nums,s,e);
+        divide(nums, 0, nums.size()-1);
         return nums;
     }
 };
