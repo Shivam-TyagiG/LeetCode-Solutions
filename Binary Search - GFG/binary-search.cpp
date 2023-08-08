0084c2a8476427614aac800475ee0b4b20c41f80
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2023-08-08 09:03:11 +0530

    Added solution - LeetHub

--- Binary Search - GFG/binary-search.cpp
+++ Binary Search - GFG/binary-search.cpp
@@ -10,18 +10,16 @@ using namespace std;
 
 class Solution {
   public:
-    int solve(int arr[], int low, int high, int k){
-        if(high<low) return -1;
-        int mid = low+high;
-        if(arr[mid]==k) return mid;
-        else if(arr[mid]<k) return solve(arr, mid+1, high, k);
-        else return solve(arr, low, mid-1, k);
-    }
     int binarysearch(int arr[], int n, int k) {
         // code here
-        int l=0;
-        int high=n-1;
-        return solve(arr, l, high, k);
+        int i=0, j=n-1;
+        while(i<=j){
+            int mid = i+(j-i)/2;
+            if(arr[mid]==k) return mid;
+            else if(arr[mid]<k) i=mid+1;
+            else j=mid-1;
+        }
+        return -1;
     }
 };
 
