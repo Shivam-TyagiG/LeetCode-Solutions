1a22a9f4ba91e46ffb283f8ae515b67a6d8fd149
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2023-08-12 23:38:15 +0530

    Added solution - LeetHub

--- Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
@@ -8,33 +8,21 @@ using namespace std;
 class Solution
 {
     public:
-    int solve(int n, int a[])
-    {
-        vector<int> curr(n+1,0);
-        vector<int> next(n+1,0);
-        for(int cur=n-1; cur>=0; cur--)
-        {
-            for(int pre = cur-1; pre>=-1; pre--)
-            {
-                int take = 0;
-                if(pre == -1 || a[cur]>a[pre])
-                {
-                    take = 1 + next[cur+1];
-                }
-                int nottake = next[pre+1];
-                curr[pre+1] = max(take,nottake);
-            }
-            next = curr;
-        }
-        return next[0];
-    }
-    
-    //Function to find length of longest increasing subsequence.
     int longestSubsequence(int n, int a[])
     {
-       // your code 
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return solve(n, a);
+       // your code here
+        vector<int> piles;
+        
+        for (int i = 0; i < n; i++) {
+            auto it = lower_bound(piles.begin(), piles.end(), a[i]);
+            if (it == piles.end()) {
+                piles.push_back(a[i]);
+            } else {
+                *it = a[i];
+            }
+        }
+        
+        return piles.size();
     }
 };
 
