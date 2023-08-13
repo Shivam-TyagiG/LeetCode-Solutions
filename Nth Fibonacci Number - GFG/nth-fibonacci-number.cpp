6bc18e3d4688950b840fe985b57001437695effa
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2023-08-13 21:38:42 +0530

    Added solution - LeetHub

--- Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
+++ Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
@@ -9,14 +9,14 @@ class Solution {
   public:
     int mod = 1000000007;
     int solve(int n, vector<int>& dp){
-        if(n<=1) return n;
+        if(n==0) return 0;
+        if(n==1) return 1;
         if(dp[n]!=-1) return dp[n];
-        dp[n] = (solve(n-1, dp)%mod+solve(n-2, dp)%mod)%mod;
-        return dp[n];
+        return dp[n] = (solve(n-1, dp)%mod + solve(n-2, dp)%mod)%mod;
     }
     int nthFibonacci(int n){
         // code here
-        vector<int> dp(n+1,-1);
+        vector<int> dp(n+1, -1);
         return solve(n, dp);
     }
 };
