9d5be7183c8ac4b2730cfd31ef72458556037ee6
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2023-09-01 20:50:37 +0530

    Time: 6 ms (56.68%), Space: 7.9 MB (65.80%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        if(n==0){
+            vector<int> dp(n+1, 0);
+            dp[0] = 0;
+            return dp;
+        }
+        if(n==1){
+            vector<int> dp(n+1, 0);
+            dp[0] = 0;
+            dp[1] = 1;
+            return dp;
+        }
+        vector<int> dp(n+1, 0);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i=2; i<=n; i++){
+            if(i%2==0){
+                dp[i] = dp[i/2];
+            }
+            else{
+                dp[i] = dp[i/2]+1;
+            }
+        }
+        return dp;
+    }
+};
