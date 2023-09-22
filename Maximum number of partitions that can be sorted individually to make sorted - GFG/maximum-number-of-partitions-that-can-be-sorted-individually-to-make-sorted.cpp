5ce3d42597537544d300f751d84a3e0893288b08
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2023-09-22 13:24:53 +0530

    Added solution - LeetHub

---
+++ Maximum number of partitions that can be sorted individually to make sorted - GFG/maximum-number-of-partitions-that-can-be-sorted-individually-to-make-sorted.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+
+// CPP program to find Maximum number of partitions
+// such that we can get a sorted array.
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to find maximum partitions.
+int maxPartitions(int arr[], int n);
+
+
+// Driver code
+int main()
+{
+	int t,n;
+	cin>>t;
+	while(t--)
+    {
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        cout << maxPartitions(arr, n)<<endl;
+    }
+
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+int maxPartitions(int arr[], int n)
+{
+    // Complete the function
+    int count=0;
+    int maxi = INT_MIN;
+    for(int i=0; i<n; i++){
+        maxi = max(arr[i], maxi);
+        if(maxi==i) count++;
+    }
+    return count ;
+    
+}
