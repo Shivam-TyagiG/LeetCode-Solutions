95844e473c79943d8ff48cca8b4fa411efe189d7
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2023-10-07 13:04:04 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -14,6 +14,10 @@ class Solution
     int celebrity(vector<vector<int> >& M, int n) 
     {
         // code here 
+        /*
+        
+        //Approach 1 
+        //the celibrity is one who have all element in a row is 0 and in column 1 except itself 
         for(int i=0; i<n; i++){
             bool possible = 1;
             for(int j=0; j<n; j++){
@@ -34,6 +38,29 @@ class Solution
             if(possible) return i;
         }
         return -1;
+        
+        */
+        
+        
+        stack<int> s;
+        for(int i=0; i<n; i++) s.push(i);
+        
+        while(s.size()>1){
+            int a = s.top();
+            s.pop();
+            int b = s.top();
+            s.pop();
+            if(M[a][b]) s.push(b);
+            else s.push(a);
+        }
+        
+        int top = s.top();
+        for(int i=0; i<n; i++){
+            if(M[top][i]==1) return -1;
+            if(i!=top && M[i][top]==0) return -1;
+        }
+        
+        return top;
     }
 };
 
