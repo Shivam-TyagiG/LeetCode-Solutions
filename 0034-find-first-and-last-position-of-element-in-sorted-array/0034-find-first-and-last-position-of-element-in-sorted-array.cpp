61a4a7c6d2e07c9decaf15121a3584d387905f29
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2023-10-09 21:20:54 +0530

    Time: 11 ms (9.25%), Space: 14.5 MB (8.57%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,60 +1,51 @@
-int firstposition(vector<int> nums,int target ,int s , int e)
-{
-    int ans=-1;
-    while(s<=e)
-    {
-        int mid = s + (e-s)/2;
-        if(nums[mid] == target)
-        {
-            ans=mid;
-            e=mid-1;
-        }
-        else if(nums[mid]>target)
-        {
-            e=mid-1;
-        }
-        else{
-            s=mid+1;
-        }
-    }
-    return ans;
-}
-
-int lastposition(vector<int> nums,int target ,int s , int e)
-{
-    int ans=-1;
-    while(s<=e)
-    {
-        int mid = s + (e-s)/2;
-        if(nums[mid] == target)
-        {
-            ans=mid;
-            s=mid+1;
-        }
-        else if(nums[mid]>target)
-        {
-            e=mid-1;
-        }
-        else{
-            s=mid+1;
-        }
-    }
-    return ans;
-}
-
-
-
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        
-        int s=0, e = nums.size() -1;
-        int a = firstposition(nums,target,s,e);
-        int b = lastposition(nums,target,s,e);
-        
-        vector<int> ans(2);
-        ans[0]=a;
-        ans[1]=b;
+    int solveleft(vector<int> arr, int target, int s, int e){
+        int ans = -1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(arr[mid]==target){
+                ans = mid;
+                e=mid-1;
+            }
+            else if(arr[mid]>target){
+                e=mid-1;
+            }
+            else{
+
+                s=mid+1;
+            }
+        }
         return ans;
+    }
+
+    int solver(vector<int> arr, int target, int s, int e){
+        int ans = -1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(arr[mid]==target){
+                ans = mid;
+                s=mid+1;
+            }
+            else if(arr[mid]>target){
+                e=mid-1;
+            }
+            else{
+
+                s=mid+1;
+            }
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int s = 0;
+        int e = nums.size()-1;
+        int a = solveleft(nums, target, s, e);
+        int b = solver(nums, target, s, e);
+        vector<int> ans;
+        ans.push_back(a);
+        ans.push_back(b);
+        return ans;
+        
     }
 };
