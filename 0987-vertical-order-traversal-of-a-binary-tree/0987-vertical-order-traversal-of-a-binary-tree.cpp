6314f95d311652d11281ad56d574810d3049f501
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2023-10-20 09:34:47 +0530

    Time: 8 ms (15.98%), Space: 12.8 MB (79.88%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,40 +12,34 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        
         vector<vector<int>> ans;
-        if(root==NULL) return ans;
-        
-        vector<int> output;
-        queue<pair<TreeNode*, pair<int,int>>> q;
-        map<int, map<int, vector<int>>> m;
-        q.push({root,{0, 0}});
-        
+        map<int, map<int,vector<int>> > m;
+        queue<pair<int,pair<int,TreeNode*>>> q;
+        if(root==NULL)  return ans;
+        q.push({0,{0, root}});
         while(!q.empty()){
-            int size = q.size();
-            for(int i=0; i<size; i++){
-                pair<TreeNode*, pair<int,int>> front = q.front();
-                q.pop();
-                TreeNode* node = front.first;
-                int hd = front.second.first;
-                int level = front.second.second;
-                m[hd][level].push_back(node->val);
-                if(node->left) q.push({node->left, {hd-1, level+1}});
-                if(node->right) q.push({node->right, {hd+1, level+1}});
-            }
+            pair<int,pair<int,TreeNode*>> front = q.front();
+            q.pop();
+            int hd = front.first;
+            int level = front.second.first;
+            TreeNode* node = front.second.second;
+            m[hd][level].push_back(node->val);
+            if(node->left) q.push({hd-1,{level+1, node->left}});
+            if(node->right) q.push({hd+1,{level+1, node->right}});
         }
-        
         for(auto i:m){
             vector<int> output;
             for(auto j:i.second){
                 sort(j.second.begin(), j.second.end());
                 for(auto k:j.second){
+                    // ans.push_back(k);
+                    // ans.push_back(j.second);
                     output.push_back(k);
                 }
             }
+            // sort(output.begin(), output.end());
             ans.push_back(output);
         }
         return ans;
-        
     }
 };
