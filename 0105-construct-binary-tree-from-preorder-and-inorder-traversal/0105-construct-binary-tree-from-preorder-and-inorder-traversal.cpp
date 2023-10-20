aa6ebe36b6df5d9efd23882f87f0931e2ee30c78
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2023-10-20 23:00:43 +0530

    Time: 15 ms (62.43%), Space: 26 MB (89.24%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -27,12 +27,37 @@ private:
         root->right = construct(pre, in, preStart, preEnd, position+1, inEnd);
         return root;
     }
+    
+    int find_index(int element, vector<int>& inorder, int in_start, int in_end){
+        for(int i=in_start; i<=in_end; i++){
+            if(element == inorder[i]) return i;
+        }
+        return -1;
+    }
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int& pre_start, int& pre_end, int in_start, int in_end){
+        if(pre_start>pre_end || in_start>in_end) return NULL;
+        int element = preorder[pre_start];
+        pre_start++;
+        int index = find_index(element, inorder, in_start, in_end);
+        TreeNode* root = new TreeNode(element);
+        root->left = solve(preorder, inorder, pre_start, pre_end, in_start, index-1);
+        root->right = solve(preorder, inorder, pre_start, pre_end, index+1, in_end);
+        return root;
+    }
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int preStart = 0;
-        int preEnd = preorder.size()-1;
-        int inStart = 0;
-        int inEnd = inorder.size()-1;
-        return construct(preorder, inorder, preStart, preEnd, inStart, inEnd);
+        // int preStart = 0;
+        // int preEnd = preorder.size()-1;
+        // int inStart = 0;
+        // int inEnd = inorder.size()-1;
+        // return construct(preorder, inorder, preStart, preEnd, inStart, inEnd);
+        
+        //construct tree
+        int pre_start = 0;
+        int pre_end = preorder.size()-1;
+        int in_start = 0;
+        int in_end = inorder.size()-1;
+        return solve(preorder, inorder, pre_start, pre_end, in_start, in_end);
+        
     }
 };
