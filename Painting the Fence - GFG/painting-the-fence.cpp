feab33bc2d6bac032cc9f4a7764a85e328b46b33
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2023-11-01 14:52:10 +0530

    Added solution - LeetHub

--- Painting the Fence - GFG/painting-the-fence.cpp
+++ Painting the Fence - GFG/painting-the-fence.cpp
@@ -23,12 +23,21 @@ class Solution{
             dp[n] = (solve1(n-2, k, dp)%mod*(k-1)%mod + solve1(n-1, k, dp)%mod*(k-1)%mod)%mod;
             return dp[n];
         }
+        long long solve2(int n, int k, vector<long long int>& dp){
+            dp[0] = 0;
+            dp[1] = k;
+            dp[2] = (k + (k*(k-1)%mod)%mod)%mod;
+            for(int i=3; i<=n; i++){
+                dp[i] = ((dp[i-2]*(k-1)%mod)%mod + (dp[i-1]*(k-1)%mod)%mod)%mod;
+            }
+            return dp[n];
+        }
     public:
     long long countWays(int n, int k){
         // code here
         // return solve(n, k);
-        vector<int> dp(n+1, -1);
-        return solve1(n, k, dp);
+        vector<long long int> dp(n+2, -1);
+        return solve2(n, k, dp);
         
     }
 };
