3dd956c290f08aad5822987a47680a1bead99612
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2024-01-17 21:37:25 +0530

    Time: 5 ms (39.16%), Space: 8.6 MB (69.36%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        int count=1;
-        int size=arr.size();
-        vector<int> temp;
-        sort(arr.begin(),arr.end());
-        for(int i=0;i<size-1;i++){
-            if(arr[i]==arr[i+1]){
-                count=count+1;
-            }
-            else{
-                temp.push_back(count);
-                count=1;
-            }
-            
-            if(i==size-2){
-                temp.push_back(count);
-            }
+
+         int size=arr.size();
+
+        unordered_map<int,int> freq;
+        for(int i=0; i<size; i++)
+        {
+            freq[arr[i]]++;
         }
-        size=temp.size();
-        sort(temp.begin(),temp.end());
-        for(int i=0;i<size-1;i++){
-            if(temp[i]==temp[i+1]){
+        
+        unordered_set<int> unique;
+        for(auto i : freq)
+        {
+            if(unique.count(i.second))
                 return false;
-            }
+            else
+                unique.insert(i.second);
         }
         return true;
-        
-        
     }
 };
