989301fec73e2f0052b16dffeb1abdf9ab33deb8
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2024-02-06 21:32:44 +0530

    Time: 23 ms (89.86%), Space: 24.5 MB (20.94%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-         unordered_map<string, vector<string>> mp;
+        unordered_map<string, vector<string>> mp;
         for (string s : strs) {
             string t = s; 
             sort(t.begin(), t.end());
@@ -13,5 +13,4 @@ public:
         }
         return anagrams;
     }
-        
 };
